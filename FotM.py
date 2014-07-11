75451f8b3079277889d4f48c582fb690e190e5d9
SheffieldPi <rclose1@sheffield.ac.uk>
2014-07-11 14:00:35 +0000

    And now, the weather

---
+++ FotM.py
@@ -0,0 +1,85 @@
+####THIS PROGRAM IS ONLY FOR THE FESTIVAL OF THE MIND EXERCISES. THE MAIN PROGRAM WITH FULL FUNCTIONALITY IS CALLED THERMODATE.PY####
+
+#These are the imports needed to get the data from the sensor.
+import time
+import Adafruit_BMP.BMP085 as BMP085
+
+#These access the WOW site to upload data
+import urllib
+import urllib2
+
+#Import the config data
+from ConfigParser import SafeConfigParser
+import sys
+
+def tempfarenheit(Temperature):
+    #WOW requires the temperature to be uploaded in Farenheit, so this will convert our values from Celcius
+    Temp = (float(Temperature)*9/5)+32
+    return Temp
+
+def presinches(pressure):
+    #WOW requires the pressure to be uploaded in inches of mercury, so this will convert our values from Pascals
+    Pres = float(pressure) * 0.000295333727
+    return Pres
+
+def Timeformatting(aTime):
+    #returns the time in both normal format and WOW format
+    Timenow = time.strftime('%Y-%m-%d %H:%M:%S',aTime)
+    Timeformat = Timenow.replace(':','%3A').replace(' ','+')
+    return Timeformat,Timenow
+
+def addvalue():
+    #This is in case the login values have not been added already
+    value = raw_input('What is your %s?' %name)
+    parser.set(section,name,value)
+
+#This checks that 
+parser = SafeConfigParser()
+parser.read('details.ini')
+
+for section in ['MetWOW']:
+    for name,value in parser.items(section):
+        if value:
+            continue
+        else:
+            addvalue()
+
+#This writes the inputted values (if any) to the file
+parser.write(open('details.ini','w'))
+
+#This assigns the values to a format that the code can now access
+AWSKey = parser.get('MetWOW','AWS_Key')
+SiteID = parser.get('MetWOW','Site_ID')
+
+sensor = BMP085.BMP085()
+softwaretype = "Sheffield-Pi-Weather-Station-0.1"
+
+#Read the data
+temp= format(sensor.read_temperature())
+pressure = format(sensor.read_pressure())
+[Timeformat,Timenow] = Timeformatting(time.gmtime())
+print "It's %s degrees Celcius where you are now!" % temp
+
+#Make it semi-interactive so kids are actually interested
+if 30> float(temp) >= 25:
+    print "That's pretty warm!"
+elif 15 <= float(temp) < 25:
+    print "That's about average temperature!"
+elif float(temp) >=30:
+    print "That's very warm. Are you warming it up with your hand? Don't try to trick me!"
+else:
+    print "Pretty chilly where you are!"
+#Get the data in the right units to upload
+Temp = tempfarenheit(temp)
+Pres = presinches(pressure)
+    
+#Construct the URL to send the data to WOW
+url = 'http://wow.metoffice.gov.uk/automaticreading?siteid=%s&siteAuthenticationKey=%s&dateutc=%s&tempf=%s&baromin=%.2f&softwaretype=%s' % (SiteID,AWSKey,Timeformat,Temp,Pres,softwaretype)
+
+#Send the request to WOW and interpret response
+request = urllib2.Request(url)
+response = urllib2.urlopen(request).getcode()
+if float(response) == 200:
+    print "You've uploaded your data to WOW!"
+else:
+    print "There was an error connecting to WOW"
