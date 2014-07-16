26cd431d7d7b663478f08360fe16bc0b0945f5a0
SheffieldPi <SheffieldPi@users.noreply.github.com>
2014-07-16 12:58:19 +0100

    Update Autorun_Station.sh

--- Autorun_Station.sh
+++ Autorun_Station.sh
@@ -2,7 +2,7 @@
 
 ### BEGIN INIT INFO
 # Provides:		Autorun_Station
-# Required-Star:	$remote_fs $syslog
+# Required-Start:	$remote_fs $syslog
 # Required-Stop:	$remote_fs $syslog
 # Default-Start:	2 3 4 5
 # Default-Stop:		0 1 6
